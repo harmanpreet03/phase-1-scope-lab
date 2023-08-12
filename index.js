7e88df3c6a1119dbcfeb03374fcb9d4f7373ad84
harmanpreet ghotra <harmanpreetghotra17@gmail.com>
2023-08-12 15:29:53 -0400

    lab done

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution in this file!
+// Declare customerName to be 'bob' in global scope
+var customerName = 'bob';
+
+// Function to modify the customerName variable to uppercase
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// Function to set the bestCustomer variable in global scope
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+// Function to overwrite the bestCustomer variable
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+// Declare a constant leastFavoriteCustomer with an initial value
+const leastFavoriteCustomer = 'someone';
+
+// Function that attempts to change the leastFavoriteCustomer constant
+function changeLeastFavoriteCustomer() {
+  // This line will throw an error due to attempting to reassign a constant
+  leastFavoriteCustomer = 'new person';
+}
+
+
+module.exports = {
+  customerName,
+  upperCaseCustomerName,
+  setBestCustomer,
+  overwriteBestCustomer,
+  changeLeastFavoriteCustomer
+};
